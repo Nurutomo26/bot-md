06dbfad4a8fd48336e8e35ed0d100c8b83dae2bb
jayabotz <107167904+jayabotz@users.noreply.github.com>
2022-06-12 20:41:42 +0800

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,4 +1,3 @@
-
 const { instagramdl } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
@@ -14,7 +13,7 @@ instagramdl(args[0]).then(async res => {
   })
 }
 handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['download']
+handler.tags = ['downloader']
 handler.command = /^(ig|igdl|instagram)$/i
 handler.limit = true
 handler.premium = false
