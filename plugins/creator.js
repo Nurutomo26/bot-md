46bc15d0f68e11b862a1de3c1ee2ceefeb46027f
jayabotz <107167904+jayabotz@users.noreply.github.com>
2022-06-13 16:12:22 +0800

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,17 +1,19 @@
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
-                    + 'VERSION:3.0\n' 
-                    + 'N:;Gulfi;;;'
-                    + 'FN:Gulfi\n' // full name
-                    + 'ORG:Gulfi;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=12053866602:+1 (205)386-6602\n' // WhatsApp ID + phone number
-                    + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Gulfi', contacts: [{ vcard }] } }, { quoted: m })
+let handler = async function (m, { conn }) {
+  let list = []
+  for (let i of owner.map(v => v + '@s.whatsapp.net')) {
+    let name = db.data.users[i] ? db.data.users[i].name : conn.getName(i)
+    list.push({
+      "displayName": name,
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+    })
+  }
+  await conn.sendMessage(m.chat, {
+    "displayName": `${list.length} Contact`,
+    "contacts": list
+  }, 'contactsArrayMessage', { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
