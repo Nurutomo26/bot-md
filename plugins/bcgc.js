0c00615bf68e7211f2992a32c721452218f76ed2
jayabotz <107167904+jayabotz@users.noreply.github.com>
2022-06-13 10:52:12 +0800

    Update bcgc.js

--- plugins/bcgc.js
+++ plugins/bcgc.js
@@ -1,21 +1,22 @@
-let handler = async (m, { conn,isOwner, isROwner, text }) => {
-    const delay = time => new Promise(res => setTimeout(res, time))
-    let getGroups = await conn.groupFetchAllParticipating()
-    let groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
-    let anu = groups.map(v => v.id)
-    let pesan = m.quoted && m.quoted.text ? m.quoted.text : text
-    if(!pesan) throw 'teksnya?'
-    m.reply(`Mengirim Broadcast Ke ${anu.length} Chat, Waktu Selesai ${anu.length * 0.5} detik`)
-    for (let i of anu) {
-    await delay(500)
-    conn.reply(i,pesan).catch(_ => _)
-    }
-  m.reply(`Sukses Mengirim Broadcast Ke ${anu.length} Group`)
+const { randomBytes } = require('crypto')
+
+let handler = async (m, { conn, text }) => {
+  let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Group Broadcast 」\n' + randomID(32)), true).catch(_ => _)
+  m.reply('Selesai Broadcast All Group :)')
 }
-handler.help = ['bcgc <teks>']
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcastgc|bcgc)$/i
+handler.command = /^(broadcast|bc)(group|grup|gc)$/i
 
 handler.owner = true
 
 module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
