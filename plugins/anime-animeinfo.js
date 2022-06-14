359831bf0a2a9660c3c68632c735be97cec738a2
jayabotz <107167904+jayabotz@users.noreply.github.com>
2022-06-14 10:22:40 +0800

    Update anime-animeinfo.js

--- plugins/anime-animeinfo.js
+++ plugins/anime-animeinfo.js
@@ -1,20 +1,31 @@
 let fetch = require('node-fetch')
-let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan query!`
-  let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
+let { JSDOM } = require('jsdom')
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Uhm.. teksnya mana?\n\nContoh:\n${usedPrefix + command} naruto`
+  let res = await fetch(API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { title, synopsis, episodes, url, rated, score, image_url } = json.results[0]
-let animeingfo = `✨️ *Title:* ${title}
+  let { title, members, synopsis, episodes, url, rated, score, image_url, type, start_date, end_date, mal_id } = json.results[0]
+  let res2 = await fetch(`https://myanimelist.net/anime/${mal_id}`)
+  if (!res2.ok) throw await res2.text()
+  let html = await res2.text()
+  let { document } = new JSDOM(html).window
+  let genAnim = [...document.querySelectorAll('div[class="spaceit_pad"] > * a')].map(el => el.href).filter(href => href.startsWith('/anime/genre/'))
+  let animeingfo = `✨️ *Title:* ${title}
 🎆️ *Episodes:* ${episodes}
+🎗️ *Genre:* ${genAnim.join(", ")}
+➡️ *Start date:* ${start_date}
+🔚 *End date:* ${end_date}
+💬 *Show Type:* ${type}
 💌️ *Rating:* ${rated}
 ❤️ *Score:* ${score}
+👥 *Members:* ${members}
 💚️ *Synopsis:* ${synopsis}
 🌐️ *URL*: ${url}`
   conn.sendFile(m.chat, image_url, '', animeingfo, m)
 }
-handler.help = ['animeinfo <judul>']
-handler.tags = ['anime']
-handler.command = /^(animeinfo)$/i
-//maapin fatur :<
+handler.help = ['anime <judul>']
+handler.tags = ['internet']
+handler.command = /^(anime|animeinfo)$/i
+
 module.exports = handler
